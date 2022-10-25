94431d3214dff4991594207ae236c58d585cb1c1
MORSALIN05 <mdmsarker04@gmail.com>
2022-10-26 01:22:37 +0600

    object bonus video 1st commit

---
+++ object.js
@@ -0,0 +1,22 @@
+const valoHoyeJaw = {
+    polite: false,
+    politeAtOutside: true,
+    Name: 'Hayat',
+    friend: ['Noman', 'humayun', 'aurongzeb', 'Basith', 'Musfiq', 'Nabab'],
+    otherJob: [{ homeWork: 'yes', helpMom: 'no' }, { earnMoney: 2000, Expense: 5000 },],
+    isReligious: function doPray() {
+        console.log('pray salah 5 times a day');
+    },
+    Phone: {
+        name: 'samsung J25',
+        price: 17000,
+        memory: '32gb',
+        color: 'silver',
+        work: {
+            callResponse: false,
+            isReading: true,
+            outSourceWork: 'trying'
+        }
+    }
+
+}
