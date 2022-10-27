a8703abc13de84d2ab4f358ef028e30218089266
MORSALIN05 <mdmsarker04@gmail.com>
2022-10-28 00:06:48 +0600

    object bonus commit3

---
+++ array.js
@@ -0,0 +1,19 @@
+const products = [
+    { name: 'phone', price: 12000 },
+    { name: 'laptop', price: 32000 },
+
+]
+products[0];
+products[1];
+
+const products1 = {
+    '0': 15,
+    '1': 34,
+    '2': 45
+}
+// const prods = products1.0; //problem
+//arguments of function
+function add(num1, num2) {
+    console.log(num1, num2);
+
+}
